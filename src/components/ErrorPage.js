73bc725059ca27cf1b3d9569b03d7cffcf25bf22
Purva Purani <purva@orientsoftware.in>
2019-02-19 19:49:55 +0530

    Initial commit

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function ErrorPage(props) {
+    
+    let int = setInterval(() => {
+        if(document.getElementById("timer")){
+            let t = parseInt(document.getElementById("timer").innerHTML);
+            document.getElementById("timer").innerHTML = t-1;
+
+            if(t===1){
+                clearInterval(int);
+                props.history.push("/");
+            }
+        }
+    }, 1000);
+
+  return (
+    <div className="container">
+    <p className="center font25">Oops! Page not found. You will be redirected to homepage in <span id="timer">5</span> seconds.<br /><img className="errorpage-img" alt="" src="images/nojs.png" /></p>
+    </div>
+  )
+}
+
+export default ErrorPage;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux'
+import rootReducer from './store/reducers/rootReducer'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+registerServiceWorker();
