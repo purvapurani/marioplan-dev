b38014fff23865d9c72526267082df0b754e77f2
Purva Purani <purvapurani@gmail.com>
2019-02-20 09:39:28 +0530

    Learning Thunk

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -7,6 +7,11 @@ const initState = {
 }
 
 const projectReducer = (state = initState, action) => {
+  switch(action.type){
+    case 'CREATE_PROJECT': {
+      console.log('Created project: ', action.project)
+    }
+  }
   return state;
 };
 
